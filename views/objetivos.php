49e7cdaefa92917d6eeb87f0c0c824f131718b47
Diego Javier Mena Amado <ingelectronicadj@gmail.com>
2015-05-17 05:43:26 -0500

    Update objetivos.php

---
+++ views/objetivos.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <div class="col-lg-9 main-chart">
+        <div class="inner cover">
+        <h1 class="cover-heading">¿Quiénes son nuestros clientes?</h1>
+        <p class="lead">Básicamente la agroindustria: Empresas, organizaciones, instituciones, comunidades y personas que participen directamente o como intermediaria en la producción agraria, procesamiento industrial o comercialización nacional y exterior de bienes agrícolas.</p>
+        
+		<h1 class="cover-heading">¿Qué quieren?</h1>
+		<p class="lead">*Monitorear los niveles de sensado involucrados en un cultivo (Temperatura, humedad relativa, humedad en el suelo, radiación lumínica, porcentajes de CO2)
+		*Manipular remotamente los sistema necesarios para un buen desarrollo del dosel vegetal (Riego, iluminación, ventilación)          
+		*Recibir alertas de estados críticos sobre el cultivo para cada estadio de la plantas.
+		*Tecnificar sus procedimientos agrícolas sin olvidar la procedencia de sus conocimientos heredados y su implicación cultural para disponer de tiempo para ofertar sus productos en el mercado de la mejor manera posible.<>/p
+		</div><br>
+    </div><!-- /col-lg-9 END SECTION MIDDLE -->
+
+</div><! --/row -->
