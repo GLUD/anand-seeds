2ec9d6f2b0b93845056a97ced0eea2cfa28e73e7
Diego Javier Mena Amado <ingelectronicadj@gmail.com>
2015-05-17 06:03:11 -0500

    Update home.php

--- views/home.php
+++ views/home.php
@@ -2,7 +2,7 @@
     <div class="col-lg-9 main-chart">
         <div class="inner cover">
         <h1 class="cover-heading">Soberanía Alimentaria.</h1>
-        <p class="lead">Es el derecho de los pueblos a definir sus propias políticas y estrategias sustentables de producción , distribucción
+        <p aling="justify" class="lead">Es el derecho de los pueblos a definir sus propias políticas y estrategias sustentables de producción , distribucción
         y consumo de alimentos que garanticen el derecho a la alimentación para toda la población , con base en la pequeña y mediana producción
         , respetando sus propias culturas y la diversidad de los modos campesinos de producción agropecuaria , de comercialización y de gestión de los
         espacios rurales en los cuales la mujer desempeña un papel fundamental</p>
