195d74f7b23f8ddafe637110d7f1f52b7b740808
Diego Javier Mena Amado <ingelectronicadj@gmail.com>
2015-05-17 05:46:57 -0500

    Update objetivos.php

--- views/objetivos.php
+++ views/objetivos.php
@@ -8,8 +8,14 @@
 		<p class="lead">*Monitorear los niveles de sensado involucrados en un cultivo (Temperatura, humedad relativa, humedad en el suelo, radiación lumínica, porcentajes de CO2)
 		*Manipular remotamente los sistema necesarios para un buen desarrollo del dosel vegetal (Riego, iluminación, ventilación)          
 		*Recibir alertas de estados críticos sobre el cultivo para cada estadio de la plantas.
-		*Tecnificar sus procedimientos agrícolas sin olvidar la procedencia de sus conocimientos heredados y su implicación cultural para disponer de tiempo para ofertar sus productos en el mercado de la mejor manera posible.<>/p
+		*Tecnificar sus procedimientos agrícolas sin olvidar la procedencia de sus conocimientos heredados y su implicación cultural para disponer de tiempo para ofertar sus productos en el mercado de la mejor manera posible.</p>
+		
+			
+		<h1 class="cover-heading">¿Nuestros clientes están dispuestos en seguir adquiriendo productos o servicios?</h1>
+		<p class="lead">Uno de los objetivos de Etnosoft es fabricar productos tecnológicos que permitan el mejoramiento de la calidad de vida desde el pequeño agricultor como del consumidor, del bienestar común o de interés social, cultural y de servicio social orientado a la soberanía alimentaria. De esa manera sus productos a vender serán construidos para garantizar altos periodos de vida útil y constantemente irán mejorando gracias a la rama de investigación de la organización. Estas mejoras vendrán a ser extensiones modulares de hardware sin modificar del todo la parte física del producto original ó actualizaciones de software de manera remota, representando así menores costos en su modernización, permitiendo una mejor respuesta a la inversión de nuevos productos.</p>
+		
 		</div><br>
+		
     </div><!-- /col-lg-9 END SECTION MIDDLE -->
 
 </div><! --/row -->
